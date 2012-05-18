efdea684c8d013b329bbb8ebfc3fa2094d39f682
Don Kirkby <donkirkby@gmail.com>
2012-05-17 21:14:13 -0700

    Read document text

--- src/live_py/LiveCodingResultsColumn.java
+++ src/live_py/LiveCodingResultsColumn.java
@@ -1,16 +1,32 @@
 package live_py;
 
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.source.CompositeRuler;
 import org.eclipse.jface.text.source.LineNumberRulerColumn;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 
 
 public class LiveCodingResultsColumn extends LineNumberRulerColumn {
+	private ITextViewer cachedTextViewer;
+	private String startText;
+
+	@Override
+	public Control createControl(CompositeRuler parentRuler,
+			Composite parentControl) {
+		cachedTextViewer= parentRuler.getTextViewer();
+		return super.createControl(parentRuler, parentControl);
+	}
+	
 	@Override
 	protected String createDisplayString(int line) {
-		return super.createDisplayString(line) + "x";
+		return super.createDisplayString(line) + startText;
 	}
 	
 	@Override
 	protected int computeNumberOfDigits() {
-		return super.computeNumberOfDigits() + 1;
+		String text = cachedTextViewer.getDocument().get();
+		startText = text.substring(0, 10);
+		return super.computeNumberOfDigits() + 10;
 	}
 }

8d4ef66bd2e84d27335332cd5486eb24f151b3d5
Don Kirkby <donkirkby@gmail.com>
2012-08-19 12:14:04 -0700

    Add example for using turtle graphics with live coding.

---
+++ test/PySrc/tools/turtle_example.py
@@ -0,0 +1,19 @@
+#echo on
+#echo width 80
+#echo turtle
+def draw(t):
+    for i in range(20):
+        d = 50 + i*10
+        t.forward(d)
+        t.right(144)
+
+if __name__ == '__live_coding__':
+    global __live_turtle__
+    draw(__live_turtle__)
+    
+elif __name__ == '__main__':
+    from Tkinter import mainloop
+    from turtle import Turtle
+    
+    draw(Turtle())
+    mainloop()

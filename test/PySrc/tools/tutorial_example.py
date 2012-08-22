06154cdb3830904e9513c75e8effede83d60462e
Don Kirkby <donkirkby@gmail.com>
2012-08-21 22:33:02 -0700

    Add tutorial example. Increment version number. Format license text.

---
+++ test/PySrc/tools/tutorial_example.py
@@ -0,0 +1,21 @@
+# echo on                               
+# echo width 40
+def search(n, a):
+    low = 0
+    high = len(a) - 1
+    while low <= high:
+        mid = (low + high) / 2
+        v = a[mid]
+        if n == v:
+            return mid
+        if n < v:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+if __name__ == '__live_coding__':
+    i = search(4, [1, 2, 4])
+
+if __name__ == '__main__':
+    print search(4, [1, 2, 4])

b78ebe9e8d41a83f180180cdf0521ff8ca865276
Don Kirkby <donkirkby@gmail.com>
2015-05-29 06:44:04 -0700

    Fix link to SWT libraries and improve turtle example.

--- test/PySrc/tools/turtle_example.py
+++ test/PySrc/tools/turtle_example.py
@@ -1,8 +1,13 @@
 def draw_spiral(t):
+    t.fillcolor('blue')
+    t.begin_fill()
     for i in range(20):
-        d = 50 + i*10
+        d = 50 + i*i*1.5
+        t.pencolor(0, 0.05*i, 0)
+        t.width(i)
         t.forward(d)
         t.right(144)
+    t.end_fill()
 
 if __name__ == '__live_coding__':
     global __live_turtle__

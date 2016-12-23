20de882a23b292bf25a28edf70f87d5a979f75bb
Don Kirkby <donkirkby@gmail.com>
2016-12-23 15:23:18 -0800

    First PyCharm plugin for #95.
    Includes side-by-side display with a copy of the source code.

---
+++ pycharm/src/io/github/donkirkby/livepycharm/SplitFileEditor.java
@@ -0,0 +1,338 @@
+package io.github.donkirkby.livepycharm;
+
+import com.intellij.codeHighlighting.BackgroundEditorHighlighter;
+import com.intellij.ide.structureView.StructureViewBuilder;
+import com.intellij.openapi.editor.ex.EditorGutterComponentEx;
+import com.intellij.openapi.fileEditor.*;
+import com.intellij.openapi.fileEditor.impl.text.PsiAwareTextEditorImpl;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.UserDataHolderBase;
+import com.intellij.openapi.wm.IdeFocusManager;
+import com.intellij.ui.JBSplitter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SplitFileEditor extends UserDataHolderBase implements FileEditor {
+    private static final Key<SplitFileEditor> PARENT_SPLIT_KEY = Key.create("parentSplit");
+
+    private static final String MY_PROPORTION_KEY = "SplitFileEditor.Proportion";
+
+    @NotNull
+    private final FileEditor myMainEditor;
+    @NotNull
+    private final FileEditor mySecondEditor;
+    @NotNull
+    private final JComponent myComponent;
+    @NotNull
+    private SplitEditorLayout mySplitEditorLayout = SplitEditorLayout.SPLIT;
+
+    @NotNull
+    private final MyListenersMultimap myListenersGenerator = new MyListenersMultimap();
+
+    SplitFileEditor(@NotNull FileEditor mainEditor, @NotNull FileEditor secondEditor) {
+        myMainEditor = mainEditor;
+        mySecondEditor = secondEditor;
+
+        myComponent = createComponent();
+
+        if (myMainEditor instanceof TextEditor) {
+            myMainEditor.putUserData(PARENT_SPLIT_KEY, this);
+        }
+        if (mySecondEditor instanceof TextEditor) {
+            mySecondEditor.putUserData(PARENT_SPLIT_KEY, this);
+        }
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return "Live coding in Python split editor";
+    }
+
+    @NotNull
+    private JComponent createComponent() {
+        final JBSplitter splitter = new JBSplitter(false, 0.5f, 0.15f, 0.85f);
+        splitter.setSplitterProportionKey(MY_PROPORTION_KEY);
+        splitter.setFirstComponent(myMainEditor.getComponent());
+        splitter.setSecondComponent(mySecondEditor.getComponent());
+
+
+//        myToolbarWrapper = new SplitEditorToolbar(splitter);
+//        if (myMainEditor instanceof TextEditor) {
+//            myToolbarWrapper.addGutterToTrack(((EditorGutterComponentEx)((TextEditor)myMainEditor).getEditor().getGutter()));
+//        }
+//        if (mySecondEditor instanceof TextEditor) {
+//            myToolbarWrapper.addGutterToTrack(((EditorGutterComponentEx)((TextEditor)mySecondEditor).getEditor().getGutter()));
+//        }
+
+        final JPanel result = new JPanel(new BorderLayout());
+//        result.add(myToolbarWrapper, BorderLayout.NORTH);
+        result.add(splitter, BorderLayout.CENTER);
+        adjustEditorsVisibility();
+
+        return result;
+    }
+
+    public void triggerLayoutChange() {
+        final int oldValue = mySplitEditorLayout.ordinal();
+        final int N = SplitEditorLayout.values().length;
+        final int newValue = (oldValue + N - 1) % N;
+
+        triggerLayoutChange(SplitEditorLayout.values()[newValue]);
+    }
+
+    private void triggerLayoutChange(@NotNull SplitFileEditor.SplitEditorLayout newLayout) {
+        if (mySplitEditorLayout == newLayout) {
+            return;
+        }
+
+        mySplitEditorLayout = newLayout;
+        invalidateLayout();
+    }
+
+    @NotNull
+    public SplitEditorLayout getCurrentEditorLayout() {
+        return mySplitEditorLayout;
+    }
+
+    private void invalidateLayout() {
+        adjustEditorsVisibility();
+        // myToolbarWrapper.refresh();
+        myComponent.repaint();
+
+        IdeFocusManager.findInstanceByComponent(myComponent).requestFocus(myComponent, true);
+    }
+
+    private void adjustEditorsVisibility() {
+        myMainEditor.getComponent().setVisible(mySplitEditorLayout.showFirst);
+        mySecondEditor.getComponent().setVisible(mySplitEditorLayout.showSecond);
+    }
+
+    @NotNull
+    public FileEditor getMainEditor() {
+        return myMainEditor;
+    }
+
+    @NotNull
+    public FileEditor getSecondEditor() {
+        return mySecondEditor;
+    }
+
+    @NotNull
+    @Override
+    public JComponent getComponent() {
+        return myComponent;
+    }
+
+    @Nullable
+    @Override
+    public JComponent getPreferredFocusedComponent() {
+        return myMainEditor.getPreferredFocusedComponent();
+    }
+
+    @NotNull
+    @Override
+    public FileEditorState getState(@NotNull FileEditorStateLevel level) {
+        return new MyFileEditorState(mySplitEditorLayout.name(), myMainEditor.getState(level), mySecondEditor.getState(level));
+    }
+
+    @Override
+    public void setState(@NotNull FileEditorState state) {
+        if (state instanceof MyFileEditorState) {
+            final MyFileEditorState compositeState = (MyFileEditorState)state;
+            if (compositeState.getFirstState() != null) {
+                myMainEditor.setState(compositeState.getFirstState());
+            }
+            if (compositeState.getSecondState() != null) {
+                mySecondEditor.setState(compositeState.getSecondState());
+            }
+            if (compositeState.getSplitLayout() != null) {
+                mySplitEditorLayout = SplitEditorLayout.valueOf(compositeState.getSplitLayout());
+                invalidateLayout();
+            }
+        }
+    }
+
+    @Override
+    public boolean isModified() {
+        return myMainEditor.isModified() || mySecondEditor.isModified();
+    }
+
+    @Override
+    public boolean isValid() {
+        return myMainEditor.isValid() && mySecondEditor.isValid();
+    }
+
+    @Override
+    public void selectNotify() {
+        myMainEditor.selectNotify();
+        mySecondEditor.selectNotify();
+    }
+
+    @Override
+    public void deselectNotify() {
+        myMainEditor.deselectNotify();
+        mySecondEditor.deselectNotify();
+    }
+
+    @Override
+    public void addPropertyChangeListener(@NotNull PropertyChangeListener listener) {
+        myMainEditor.addPropertyChangeListener(listener);
+        mySecondEditor.addPropertyChangeListener(listener);
+
+        final DoublingEventListenerDelegate delegate = myListenersGenerator.addListenerAndGetDelegate(listener);
+        myMainEditor.addPropertyChangeListener(delegate);
+        mySecondEditor.addPropertyChangeListener(delegate);
+    }
+
+    @Override
+    public void removePropertyChangeListener(@NotNull PropertyChangeListener listener) {
+        myMainEditor.removePropertyChangeListener(listener);
+        mySecondEditor.removePropertyChangeListener(listener);
+
+        final DoublingEventListenerDelegate delegate = myListenersGenerator.removeListenerAndGetDelegate(listener);
+        if (delegate != null) {
+            myMainEditor.removePropertyChangeListener(delegate);
+            mySecondEditor.removePropertyChangeListener(delegate);
+        }
+    }
+
+    @Nullable
+    @Override
+    public BackgroundEditorHighlighter getBackgroundHighlighter() {
+        return myMainEditor.getBackgroundHighlighter();
+    }
+
+    @Nullable
+    @Override
+    public FileEditorLocation getCurrentLocation() {
+        return myMainEditor.getCurrentLocation();
+    }
+
+    @Nullable
+    @Override
+    public StructureViewBuilder getStructureViewBuilder() {
+        return myMainEditor.getStructureViewBuilder();
+    }
+
+    @Override
+    public void dispose() {
+        Disposer.dispose(myMainEditor);
+        Disposer.dispose(mySecondEditor);
+    }
+
+    static class MyFileEditorState implements FileEditorState {
+        @Nullable
+        private final String mySplitLayout;
+        @Nullable
+        private final FileEditorState myFirstState;
+        @Nullable
+        private final FileEditorState mySecondState;
+
+        MyFileEditorState(@Nullable String splitLayout, @Nullable FileEditorState firstState, @Nullable FileEditorState secondState) {
+            mySplitLayout = splitLayout;
+            myFirstState = firstState;
+            mySecondState = secondState;
+        }
+
+        @Nullable
+        String getSplitLayout() {
+            return mySplitLayout;
+        }
+
+        @Nullable
+        FileEditorState getFirstState() {
+            return myFirstState;
+        }
+
+        @Nullable
+        FileEditorState getSecondState() {
+            return mySecondState;
+        }
+
+        @Override
+        public boolean canBeMergedWith(FileEditorState otherState, FileEditorStateLevel level) {
+            return otherState instanceof MyFileEditorState
+                    && (myFirstState == null || myFirstState.canBeMergedWith(((MyFileEditorState)otherState).myFirstState, level))
+                    && (mySecondState == null || mySecondState.canBeMergedWith(((MyFileEditorState)otherState).mySecondState, level));
+        }
+    }
+
+    private class DoublingEventListenerDelegate implements PropertyChangeListener {
+        @NotNull
+        private final PropertyChangeListener myDelegate;
+
+        private DoublingEventListenerDelegate(@NotNull PropertyChangeListener delegate) {
+            myDelegate = delegate;
+        }
+
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            myDelegate.propertyChange(new PropertyChangeEvent(SplitFileEditor.this, evt.getPropertyName(), evt.getOldValue(), evt.getNewValue()));
+        }
+    }
+
+    private class MyListenersMultimap {
+        private final Map<PropertyChangeListener, Pair<Integer, DoublingEventListenerDelegate>> myMap =
+                new HashMap<>();
+
+        @NotNull
+        DoublingEventListenerDelegate addListenerAndGetDelegate(@NotNull PropertyChangeListener listener) {
+            if (!myMap.containsKey(listener)) {
+                myMap.put(listener, Pair.create(1, new DoublingEventListenerDelegate(listener)));
+            }
+            else {
+                final Pair<Integer, DoublingEventListenerDelegate> oldPair = myMap.get(listener);
+                myMap.put(listener, Pair.create(oldPair.getFirst() + 1, oldPair.getSecond()));
+            }
+
+            return myMap.get(listener).getSecond();
+        }
+
+        @Nullable
+        DoublingEventListenerDelegate removeListenerAndGetDelegate(@NotNull PropertyChangeListener listener) {
+            final Pair<Integer, DoublingEventListenerDelegate> oldPair = myMap.get(listener);
+            if (oldPair == null) {
+                return null;
+            }
+
+            if (oldPair.getFirst() == 1) {
+                myMap.remove(listener);
+            }
+            else {
+                myMap.put(listener, Pair.create(oldPair.getFirst() - 1, oldPair.getSecond()));
+            }
+            return oldPair.getSecond();
+        }
+    }
+
+    public enum SplitEditorLayout {
+        FIRST(true, false, "X"),
+        SECOND(false, true, "Y"),
+        SPLIT(true, true, "Z");
+
+        public final boolean showFirst;
+        public final boolean showSecond;
+        public final String presentationName;
+
+        SplitEditorLayout(boolean showFirst, boolean showSecond, String presentationName) {
+            this.showFirst = showFirst;
+            this.showSecond = showSecond;
+            this.presentationName = presentationName;
+        }
+
+        @Override
+        public String toString() {
+            return presentationName;
+        }
+    }
+}
---
+++ pycharm/src/io/github/donkirkby/livepycharm/SplitFileEditorProvider.java
@@ -0,0 +1,150 @@
+package io.github.donkirkby.livepycharm;
+
+import com.intellij.openapi.fileEditor.*;
+import com.intellij.openapi.fileEditor.impl.FileEditorProviderManagerImpl;
+import com.intellij.openapi.fileEditor.impl.text.PsiAwareTextEditorProvider;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.jetbrains.python.PythonFileType;
+import org.jdom.Attribute;
+import org.jdom.Element;
+import org.jetbrains.annotations.NotNull;
+
+public class SplitFileEditorProvider implements AsyncFileEditorProvider {
+
+    private static final String FIRST_EDITOR = "first_editor";
+    private static final String SECOND_EDITOR = "second_editor";
+    private static final String SPLIT_LAYOUT = "split_layout";
+
+    @NotNull
+    private final com.intellij.openapi.fileEditor.FileEditorProvider myFirstProvider;
+    @NotNull
+    private final com.intellij.openapi.fileEditor.FileEditorProvider mySecondProvider;
+
+    @NotNull
+    private final String myEditorTypeId;
+
+    public SplitFileEditorProvider() {
+        myFirstProvider = new PsiAwareTextEditorProvider();
+        mySecondProvider = new PsiAwareTextEditorProvider(); // TODO: new display
+
+        myEditorTypeId = "live-pycharm";
+    }
+
+    @Override
+    public boolean accept(@NotNull Project project, @NotNull VirtualFile file) {
+        FileType fileType = file.getFileType();
+        return myFirstProvider.accept(project, file) &&
+                mySecondProvider.accept(project, file) &&
+                fileType == PythonFileType.INSTANCE;
+    }
+
+    @NotNull
+    @Override
+    public FileEditor createEditor(@NotNull Project project, @NotNull VirtualFile file) {
+        return createEditorAsync(project, file).build();
+    }
+
+    @NotNull
+    @Override
+    public String getEditorTypeId() {
+        FileEditorProviderManagerImpl x = null;
+        return myEditorTypeId;
+    }
+
+    @NotNull
+    @Override
+    public Builder createEditorAsync(@NotNull final Project project, @NotNull final VirtualFile file) {
+        final Builder firstBuilder = getBuilderFromEditorProvider(myFirstProvider, project, file);
+        final Builder secondBuilder = getBuilderFromEditorProvider(mySecondProvider, project, file);
+
+        return new Builder() {
+            @Override
+            public FileEditor build() {
+                return createSplitEditor(firstBuilder.build(), secondBuilder.build());
+            }
+        };
+    }
+
+    @NotNull
+    @Override
+    public FileEditorState readState(@NotNull Element sourceElement, @NotNull Project project, @NotNull VirtualFile file) {
+        Element child = sourceElement.getChild(FIRST_EDITOR);
+        FileEditorState firstState = null;
+        if (child != null) {
+            firstState = myFirstProvider.readState(child, project, file);
+        }
+        child = sourceElement.getChild(SECOND_EDITOR);
+        FileEditorState secondState = null;
+        if (child != null) {
+            secondState = mySecondProvider.readState(child, project, file);
+        }
+
+        final Attribute attribute = sourceElement.getAttribute(SPLIT_LAYOUT);
+
+        final String layoutName;
+        if (attribute != null) {
+            layoutName = attribute.getValue();
+        }
+        else {
+            layoutName = null;
+        }
+
+        return new SplitFileEditor.MyFileEditorState(layoutName, firstState, secondState);
+    }
+
+    @Override
+    public void writeState(@NotNull FileEditorState state, @NotNull Project project, @NotNull Element targetElement) {
+        if (!(state instanceof SplitFileEditor.MyFileEditorState)) {
+            return;
+        }
+        final SplitFileEditor.MyFileEditorState compositeState = (SplitFileEditor.MyFileEditorState)state;
+
+        Element child = new Element(FIRST_EDITOR);
+        if (compositeState.getFirstState() != null) {
+            myFirstProvider.writeState(compositeState.getFirstState(), project, child);
+            targetElement.addContent(child);
+        }
+
+        child = new Element(SECOND_EDITOR);
+        if (compositeState.getSecondState() != null) {
+            mySecondProvider.writeState(compositeState.getSecondState(), project, child);
+            targetElement.addContent(child);
+        }
+
+        if (compositeState.getSplitLayout() != null) {
+            targetElement.setAttribute(SPLIT_LAYOUT, compositeState.getSplitLayout());
+        }
+    }
+
+    private FileEditor createSplitEditor(
+            @NotNull final FileEditor firstEditor,
+            @NotNull FileEditor secondEditor) {
+        return new SplitFileEditor(firstEditor, secondEditor);
+    }
+
+    @NotNull
+    @Override
+    public FileEditorPolicy getPolicy() {
+        return FileEditorPolicy.HIDE_DEFAULT_EDITOR;
+    }
+
+    @NotNull
+    private static Builder getBuilderFromEditorProvider(@NotNull final com.intellij.openapi.fileEditor.FileEditorProvider provider,
+                                                        @NotNull final Project project,
+                                                        @NotNull final VirtualFile file) {
+        if (provider instanceof AsyncFileEditorProvider) {
+            return ((AsyncFileEditorProvider)provider).createEditorAsync(project, file);
+        }
+        else {
+            return new Builder() {
+                @Override
+                public FileEditor build() {
+                    return provider.createEditor(project, file);
+                }
+            };
+        }
+    }
+}
+

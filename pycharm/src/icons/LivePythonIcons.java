1e01bee29bcdf2e51afa1d6c0c632faa7dff19c8
Don Kirkby <donkirkby@gmail.com>
2017-06-01 22:29:28 -0700

    Close #117 by adding start and stop buttons in PyCharm.

---
+++ pycharm/src/icons/LivePythonIcons.java
@@ -0,0 +1,10 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface LivePythonIcons {
+    Icon MEDIA_PLAY = IconLoader.getIcon("/icons/media-play-2x.png");
+    Icon MEDIA_STOP = IconLoader.getIcon("/icons/media-stop-2x.png");
+}

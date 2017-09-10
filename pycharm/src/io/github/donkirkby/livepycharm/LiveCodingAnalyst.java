36ce7439ff1afb26fcd0435c0e9c8416fe637488
Don Kirkby <donkirkby@users.noreply.github.com>
2017-09-10 10:44:23 -0700

    Fix #124 by moving analysis to a background task.
    Also avoids blocking the UI when Python code is slow to execute.

--- pycharm/src/io/github/donkirkby/livepycharm/LiveCodingAnalyst.java
+++ pycharm/src/io/github/donkirkby/livepycharm/LiveCodingAnalyst.java
@@ -8,15 +8,24 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.event.DocumentAdapter;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.progress.ProcessCanceledException;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.util.ProgressIndicatorUtils;
+import com.intellij.openapi.progress.util.ReadTask;
 import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
+import java.util.concurrent.*;
 
 public class LiveCodingAnalyst extends DocumentAdapter {
     private static Logger log = Logger.getInstance(LiveCodingAnalyst.class);
+    private static ExecutorService pool = Executors.newCachedThreadPool();
     private final VirtualFile mainFile;
     private final Document displayDocument;
     private boolean isRunning;
+    private String prevSourceCode;  // last source code analysed without being cancelled
 
     LiveCodingAnalyst(VirtualFile mainFile, Document displayDocument, Disposable parent) {
         this.mainFile = mainFile;
@@ -36,12 +45,48 @@ public class LiveCodingAnalyst extends DocumentAdapter {
         isRunning = false;
     }
 
+    private class AnalysisTask extends ReadTask {
+        private Document document;
+
+        AnalysisTask(Document document) {
+            this.document = document;
+        }
+
+        @Nullable
+        @Override
+        public Continuation performInReadAction(@NotNull ProgressIndicator indicator) throws ProcessCanceledException {
+            String sourceCode = document.getText();
+            String display = analyseDocument(sourceCode, indicator);
+            if (display == null) {
+                return null;
+            }
+            return new Continuation(() -> displayResult(display));
+        }
+
+        @Override
+        public void onCanceled(@NotNull ProgressIndicator indicator) {
+            ProgressIndicatorUtils.scheduleWithWriteActionPriority(new AnalysisTask(document));
+        }
+    }
+
     @Override
     public void documentChanged(DocumentEvent e) {
         if ( ! isRunning) {
             return;
         }
-        StringBuilder builder = new StringBuilder();
+        ProgressIndicatorUtils.scheduleWithWriteActionPriority(new AnalysisTask(e.getDocument()));
+    }
+
+    private void displayResult(String display) {
+        ApplicationManager.getApplication().runWriteAction(() -> displayDocument.setText(display));
+    }
+
+    @Nullable
+    private synchronized String analyseDocument(String source, ProgressIndicator indicator) {
+        if (source.equals(prevSourceCode)) {
+            return null;
+        }
+        String output = "";
         File plugins = new File(PathManager.getPluginsPath());
         File livePyPath = new File(plugins, "livepy");
         File pythonPath;
@@ -64,21 +109,42 @@ public class LiveCodingAnalyst extends DocumentAdapter {
         try {
             Process process = processBuilder.start();
             OutputStream outputStream = process.getOutputStream();
-            outputStream.write(e.getDocument().getText().getBytes());
+            outputStream.write(source.getBytes());
             outputStream.close();
-            InputStream inputStream = process.getInputStream();
-            BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(inputStream));
-            String line;
-            while (null != (line = reader.readLine())) {
-                builder.append(line);
-                builder.append("\n");
+            Future<String> reader = pool.submit(() -> readOutput(process));
+            boolean isWaiting = true;
+            while (isWaiting) {
+                try {
+                    output = reader.get(50, TimeUnit.MILLISECONDS);
+                    isWaiting = false;
+                } catch (TimeoutException e) {
+                    try {
+                        indicator.checkCanceled();
+                    } catch (ProcessCanceledException ex) {
+                        process.destroy();
+                        throw ex;
+                    }
+                }
             }
-        } catch (IOException ex) {
+        } catch (IOException | InterruptedException | ExecutionException ex) {
             log.error("Report failed.", ex);
         }
 
-        ApplicationManager.getApplication().runWriteAction(
-                () -> displayDocument.setText(builder.toString()));
+        prevSourceCode = source;
+        return output;
+    }
+
+    @NotNull
+    private String readOutput(Process process) throws IOException {
+        InputStream inputStream = process.getInputStream();
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder builder = new StringBuilder();
+        String line;
+        while (null != (line = reader.readLine())) {
+            builder.append(line);
+            builder.append("\n");
+        }
+        return builder.toString();
     }
 }

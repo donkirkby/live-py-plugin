da381728f647611310b14691e6c1e9089d20706c
Don Kirkby <donkirkby@gmail.com>
2017-11-09 20:07:13 -0800

    Update version numbers and publish new Eclipse release.

--- emacs-live-py-mode/live-py-mode.el
+++ emacs-live-py-mode/live-py-mode.el
@@ -6,7 +6,7 @@
 ;; Keywords: live coding
 ;; URL: http://donkirkby.github.io/live-py-plugin/
 ;; Version: 2017
-;; X-Original-Version: 2.18
+;; X-Original-Version: 2.19
 ;; Package-Requires: ((emacs "24.3"))
 
 ;; This program is distributed under the MIT License

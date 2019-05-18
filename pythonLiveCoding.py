20c6958012b48ab059dc5c6c852298396fb71eb9
Derfies <Derfies@users.noreply.github.com>
2019-05-18 17:53:08 +1000

    [EDIT] - Renamed "start pyglet" command to "start canvas" as this seems more appropriate. [EDIT] - Main plugin has got some refactoring love. Now that the main call to subprocess is inside an async callback the experience is a lot smoother :) [EDIT] - Implemented CanvasReader and CanvasCommand classes which are ports from those in the pycharm plugin. [EDIT] - Now showing the goal image using a phantom via data uri. Seems to work pretty well!

--- pythonLiveCoding.py
+++ pythonLiveCoding.py
@@ -1,8 +1,5 @@
-import os
 import io
-import sys
 import logging
-import tempfile
 import functools
 import subprocess
 
@@ -10,11 +7,8 @@ import sublime, sublime_plugin
 
 
 XMIN, YMIN, XMAX, YMAX = list(range(4))
-OUTPUT_TEXT = 'output_text'
-OUTPUT_IMAGE = 'output_image'
-OUTPUT_TYPE = 'output_type'
 OUTPUT_VIEW_ID = 'output_view_id'
-
+INPUT_VIEW_TRACER_ARGS = 'input_view_tracer_args'
 CANVAS_START = 'start_canvas'
 CANVAS_END = 'end_canvas'
 
@@ -30,28 +24,140 @@ def find_view(view_id):
     return None
 
 
+def trace_code(input, pargs=None):
+
+    # Pull location of python exe and code_tracer.py script from user settings.
+    settings = sublime.load_settings('PythonLiveCoding.sublime-settings')
+    py_path = settings.get('python_executable')
+    tracer_path = settings.get('code_tracer')
+    args = [py_path, tracer_path]
+    if pargs is not None:
+        args.extend(pargs)
+    args.append('-')
+
+    # Startup info so we don't open a new command prompt.
+    startupinfo = subprocess.STARTUPINFO()
+    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+    # Launch, pipe in code and return.
+    proc = subprocess.Popen(
+        args, 
+        stdin=subprocess.PIPE, 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE, 
+        startupinfo=startupinfo,
+        universal_newlines=True
+    )
+    return proc.communicate(input=input)
+
+
+def update_view(output_view, out, err):
+
+    img = None
+    reader = io.StringIO(out)
+    stdout = ''
+
+    done = False
+    while not done:
+        line = reader.readline()
+        if line.strip() == CANVAS_START:
+            cmds = CanvasReader(reader).read_commands()
+            for cmd in cmds:
+                if cmd.name == 'create_image':
+                    encoded = cmd.options['image']
+                    img_src = 'data:image/png;base64,{}'.format(encoded)
+                    img = '<img src="{}" width="800" height="600">'.format(img_src)
+            line = reader.readline() # Skip a line.
+        elif line:
+            stdout += line
+        else:
+            done = True
+
+    output_view.erase_phantoms('goal_image')
+    if img is not None:
+        output_view.run_command('output_view_replace', {'text': ''})
+        output_view.add_phantom('goal_image', output_view.sel()[0], img, sublime.LAYOUT_BLOCK)
+    else:
+        output_view.run_command('output_view_replace', {'text': stdout})
+
+
 class CanvasCommand(object):
-    pass
+    
+    def __init__(self, *args, **kwargs):
+        self.name = None
+        self.coordinates = []
+        self.options = {}
     
 
 class CanvasReader(object):
 
-    def __init__(self, inputReader):
-        self.inputReader = inputReader
+    def __init__(self, input_reader):
+        self.input_reader = input_reader
+        self.next_line = None
 
     def read(self):
-        return self.inputReader.readline()
+        line = self.next_line if self.next_line is not None else self.input_reader.readline().rstrip()
+        if line is None:
+            return None
+        self.next_line = None
+        command = CanvasCommand()
+        command.name = line
+        while True:
+            self.next_line = self.input_reader.readline().rstrip()
+            if self.next_line is None or not self.next_line.startswith('    '):
+                return command
+            self.next_line = self.next_line[4:] # trim spaces
+            if '=' not in self.next_line:
+                command.coordinates.append(int(self.next_line))
+            else:
+                position = self.next_line.index('=')
+                name = self.next_line[:position]
+                value = self.next_line[position + 1:]
+                value = self.unescape_string(value)
+                command.options[name] = value
 
-    def readCommands(self):
-        newCommands = []
+    def unescape_string(self, value):
+        if value.startswith('\'') and value.endswith('\''):
+            value = value[1:len(value) - 1]
+            writer = ''
+            for i, c in enumerate(value):
+                if c != '\\' or i == len(value) - 1:
+                    writer += c
+                else:
+                    i += 1
+                    c2 = value[i]
+                    if c2 == '\\':
+                        writer += '\\'
+                        break
+                    elif c2 == 'n':
+                        writer += '\n'
+                        break
+                    elif c2 == 'r':
+                        writer += '\r'
+                        break
+                    elif c2 == 't':
+                        writer += '\t'
+                        break
+                    elif c2 == 'x':
+                        if i + 2 < len(value):
+                            x = int(value[i + 1:i + 3])
+                            i += 2
+                            break
+                    else:
+                        writer += c
+                        writer += c2
+                        break;
+        return value
+
+    def read_commands(self):
+        new_commands = []
         done = False
         while not done:
             command = self.read()
-            print('command:', command)
-            done = command == None or command.strip() == CANVAS_END
+            done = command is None or command.name == CANVAS_END
             if not done:
-               newCommands.append(command)
-        return newCommands
+               new_commands.append(command)
+        return new_commands
 
 
 class BaseWindowCommand(sublime_plugin.WindowCommand):
@@ -124,7 +230,7 @@ class ResetCommand(BaseWindowCommand):
 
 class BaseStartCommand(BaseWindowCommand):
 
-    OUTPUT_TYPE = None
+    TRACER_ARGS = []
 
     def run(self):
         active_group = self.window.active_group()
@@ -135,90 +241,32 @@ class BaseStartCommand(BaseWindowCommand):
             return
 
         output_view = self.create_pane()
+        output_view.set_scratch(True)
+        output_view.settings().set('word_wrap', False)
+        output_view.set_name('live coding output')
 
         input_view.settings().set('word_wrap', False)
         input_view.settings().set(OUTPUT_VIEW_ID, output_view.id())
-        
-        output_view.set_scratch(True)
-        output_view.settings().set('word_wrap', False)
-        output_view.settings().set(OUTPUT_TYPE, self.OUTPUT_TYPE)
-        output_view.set_name('live coding output')
-        
-        output_view.run_command('output_view_update', {'input_view_id': input_view.id()})
+        input_view.settings().set(INPUT_VIEW_TRACER_ARGS, self.TRACER_ARGS)
+
+        contents = input_view.substr(sublime.Region(0, input_view.size()))
+        update_view(output_view, *trace_code(contents, self.TRACER_ARGS))
 
 
 class StartCommand(BaseStartCommand):
 
-    OUTPUT_TYPE = OUTPUT_TEXT
+    pass
 
 
-class PygletCommand( BaseStartCommand ):
+class StartCanvasCommand( BaseStartCommand ):
 
-    OUTPUT_TYPE = OUTPUT_IMAGE
+    TRACER_ARGS = ['--canvas']
 
 
-class OutputViewUpdateCommand(sublime_plugin.TextCommand):
+class OutputViewReplaceCommand(sublime_plugin.TextCommand):
 
-    def trace_code(self, contents):
-
-        # Pull location of python exe and code_tracer.py script from user
-        # settings.
-        settings = sublime.load_settings('PythonLiveCoding.sublime-settings')
-        py_path = settings.get('python_executable')
-        tracer_path = settings.get('code_tracer')
-        args = [
-            py_path, 
-            tracer_path,
-            '--canvas',
-            '-'
-        ]
-
-        # Startup info so we don't open a new command prompt.
-        startupinfo = subprocess.STARTUPINFO()
-        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-
-        # Launch, pipe in code and return.
-        proc = subprocess.Popen(
-            args, 
-            stdin=subprocess.PIPE, 
-            stdout=subprocess.PIPE, 
-            stderr=subprocess.PIPE, 
-            startupinfo=startupinfo,
-            universal_newlines=True
-        )
-        out, err = proc.communicate(input=contents)
-
-        reader = io.StringIO(out)
-        stdout = ''
-        done = False
-        while not done:
-            line = reader.readline()
-            if line.strip() == CANVAS_START:
-                cmds = CanvasReader(reader).readCommands()
-                line = reader.readline() # Skip a line.
-            elif line:
-                stdout += line
-            else:
-                done = True
-        return stdout
-
-    def run(self, edit, input_view_id=None):
-        input_view = find_view(input_view_id)
-        logging.getLogger().info('Update running for input view id: {}'.format(self.view.id()))
-        contents = input_view.substr(sublime.Region(0, input_view.size()))
-        code_report = self.trace_code(contents)
-        output_type = self.view.settings().get(OUTPUT_TYPE, OUTPUT_TEXT)
-        if output_type == OUTPUT_TEXT:
-            logging.getLogger().info('Updating text in view id: {}'.format(self.view.id()))
-            self.view.replace(edit, sublime.Region(0, self.view.size()), code_report)
-        else:
-            logging.getLogger().info('Updating image in view id: {}'.format(self.view.id()))
-
-            self.view.erase(edit, sublime.Region(0, self.view.size()))
-            img_path = 'file://{}'.format(os.path.join(tempfile.gettempdir(), 'screenshot.png'))
-            html = '<img src="' + img_path + '" width="800" height="600">'
-            self.view.erase_phantoms('test')
-            self.view.add_phantom('test', self.view.sel()[0], html, sublime.LAYOUT_BLOCK)
+    def run(self, edit, text=''):
+        self.view.replace(edit, sublime.Region(0, self.view.size()), text)
 
 
 class InputViewEventListener(sublime_plugin.ViewEventListener):
@@ -234,22 +282,17 @@ class InputViewEventListener(sublime_plugin.ViewEventListener):
     def is_applicable(cls, settings):
         return settings.has(OUTPUT_VIEW_ID)
 
-    def on_timeout(self):
+    def on_timeout_async(self):
         self.pending = self.pending - 1
         if self.pending == 0:
 
-            # There are no more queued up calls to on_timeout, so it must 
-            # have been 1000ms since the last modification.
-            settings = sublime.load_settings('PythonLiveCoding.sublime-settings')
-            timeout = settings.get('timout_duration', 300)
-            logging.getLogger().info('Window idle for: {}ms - updating live coding output'.format(timeout))
-            self.on_idle()
-
-    def on_idle(self):
-        """Update the output view."""
-        settings = self.view.settings()
-        output_view = find_view(settings.get(OUTPUT_VIEW_ID))
-        output_view.run_command('output_view_update', {'input_view_id': self.view.id()})
+            # There are no more queued up calls to on_timeout, so it must have
+            # been 1000ms since the last modification.
+            view_settings = self.view.settings()
+            output_view = find_view(view_settings.get(OUTPUT_VIEW_ID))
+            contents = self.view.substr(sublime.Region(0, self.view.size()))
+            tracer_args = view_settings.get(INPUT_VIEW_TRACER_ARGS)
+            update_view(output_view, *trace_code(contents, tracer_args))
 
     def on_modified_async(self):
         self.pending = self.pending + 1
@@ -257,4 +300,4 @@ class InputViewEventListener(sublime_plugin.ViewEventListener):
         # Ask for on_timeout to be called when timeout has expired.
         settings = sublime.load_settings('PythonLiveCoding.sublime-settings')
         timeout = settings.get('timout_duration', 300)
-        sublime.set_timeout(functools.partial(self.on_timeout), timeout)
+        sublime.set_timeout_async(functools.partial(self.on_timeout_async), timeout)

883a89b9a131d8927e84574dee691df633cbac5a
Don Kirkby <donkirkby@gmail.com>
2019-06-02 18:28:43 -0700

    Add unit tests for Pyglet support, to close #229.

--- test/PySrc/tests/test_code_tracer_matplotlib.py
+++ test/PySrc/tests/test_code_tracer_matplotlib.py
@@ -20,6 +20,18 @@ def clear_matplotlib():
             yield True
 
 
+@pytest.fixture(name='is_pyglet_cleared')
+def clear_pyglet():
+    for should_yield in (True, False):
+        to_delete = [module_name
+                     for module_name in sys.modules
+                     if module_name.startswith('pyglet')]
+        for module_name in to_delete:
+            del sys.modules[module_name]
+        if should_yield:
+            yield True
+
+
 def replace_image(report):
     report = trim_report(report)
     report = re.sub(r"image='[a-zA-Z0-9+/=]*'", "image='...'", report)
@@ -62,3 +74,67 @@ x = np.random.normal(size=3)
     for _ in range(5):
         report = tracer.trace_code(code)
         assert original_report == report
+
+
+def test_pyglet_variables(is_pyglet_cleared):
+    assert is_pyglet_cleared
+    code = """\
+import pyglet.text
+
+window = pyglet.window.Window()
+label = pyglet.text.Label('Hello, world')
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+pyglet.app.run()
+"""
+    expected_report = """\
+
+
+window = MockPygletWindow(width=100, height=100)
+
+
+
+
+
+
+
+"""
+    canvas = Canvas(width=100, height=100)
+    tracer = CodeTracer(canvas)
+
+    report = tracer.trace_code(code)
+
+    assert expected_report == trim_report(report)
+
+
+def test_pyglet_window(is_pyglet_cleared):
+    assert is_pyglet_cleared
+    code = """\
+import pyglet
+
+window = pyglet.window.Window()
+label = pyglet.text.Label('Hello, world')
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+pyglet.app.run()
+"""
+    expected_report = """\
+create_image
+    0
+    0
+    image='...'
+"""
+    canvas = Canvas(width=100, height=100)
+    tracer = CodeTracer(canvas)
+
+    report = tracer.trace_turtle(code)
+
+    assert expected_report == replace_image(report)

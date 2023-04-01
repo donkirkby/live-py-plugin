0514f0168d0e9680f3fd8c3f89f7226d7eb64ef7
Don Kirkby <donkirkby@gmail.com>
2023-03-31 21:09:26 -0700

    Highlight browser diffs in red/yellow, for #520.
    Also upgrade pyodide deploy script to be compatible with a new version of pyodide.

--- html/deploy.js
+++ html/deploy.js
@@ -41,25 +41,27 @@ function copyIndex(indexSrcPath, destFolderPath) {
 function copyPyodide(srcPath, destPath) {
     const srcFiles = [
         'cycler-0.11.0-py3-none-any.whl',
-        'distutils.tar',
-        'fonttools-4.32.0-py3-none-any.whl',
-        'kiwisolver-1.4.2-cp310-cp310-emscripten_wasm32.whl',
-        'matplotlib-3.5.1-cp310-cp310-emscripten_wasm32.whl',
-        'numpy-1.22.3-cp310-cp310-emscripten_wasm32.whl',
+        'distutils-1.0.0.zip',
+        'fonttools-4.38.0-py3-none-any.whl',
+        'kiwisolver-1.4.4-cp310-cp310-emscripten_3_1_27_wasm32.whl',
+        'matplotlib-3.5.2-cp310-cp310-emscripten_3_1_27_wasm32.whl',
+        'matplotlib_pyodide-0.1.1-py3-none-any.whl',
+        'numpy-1.23.5-cp310-cp310-emscripten_3_1_27_wasm32.whl',
         'packaging-21.3-py3-none-any.whl',
-        'PIL-9.1.0-cp310-cp310-emscripten_wasm32.whl',
+        'PIL-9.1.1-cp310-cp310-emscripten_3_1_27_wasm32.whl',
         'pyodide.js',
         'pyodide.js.map',
         'pyodide.asm.wasm',
         'pyodide.asm.js',
         'pyodide.asm.data',
         'pyodide_py.tar',
-        'pyparsing-3.0.7-py3-none-any.whl',
+        'pyparsing-3.0.9-py3-none-any.whl',
         'python_dateutil-2.8.2-py2.py3-none-any.whl',
-        'pytz-2022.1-py2.py3-none-any.whl',
-        'packages.json',
+        'pytz-2022.7-py2.py3-none-any.whl',
+        'package.json',
+        'repodata.json',
         'six-1.16.0-py2.py3-none-any.whl',
-        'space_tracer-4.9.0-py3-none-any.whl'];
+        'space_tracer-4.9.1-py3-none-any.whl'];
     fs.mkdirSync(destPath);
     for (const fileName of srcFiles) {
         const fileSrcPath = path.join(srcPath, fileName),
@@ -72,7 +74,7 @@ function main() {
     const dest = path.resolve('../docs/demo'),
         src = path.resolve('build'),
         pyodideDest = path.resolve('../docs/demo/pyodide'),
-        pyodideSrc = path.resolve('../../pyodide/build'),
+        pyodideSrc = path.resolve('../../pyodide/dist'),
         pyodideExists = fs.existsSync(pyodideSrc),
         d = fs.opendirSync(dest);
     let entry;

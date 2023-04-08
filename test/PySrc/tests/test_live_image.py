f0effde83eabae5090a362ddb177b47dd10e487b
Don Kirkby <donkirkby@gmail.com>
2023-04-07 22:59:27 -0700

    Close #360 by removing try/except from test.

--- test/PySrc/tests/test_live_image.py
+++ test/PySrc/tests/test_live_image.py
@@ -246,6 +246,7 @@ def test_differ_compare():
     assert diff_pixel2 == expected_diff
     assert differ.diff_count == 1
 
+
 @pytest.mark.skipif(Image is None, reason='Pillow not installed.')
 def test_compare_pixel_alpha_channel_missing():
 
@@ -253,10 +254,7 @@ def test_compare_pixel_alpha_channel_missing():
     image2 = LivePillowImage(Image.new('RGB', (100, 200)))
 
     differ = LiveImageDiffer()
-    try:
-        differ.compare(image1, image2)
-    except Exception as e:
-        assert False, f"Raised an exception: {e}"
+    differ.compare(image1, image2)
 
 
 # noinspection DuplicatedCode

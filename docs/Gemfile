2fc81785dde855549067cdbf54f4f521faada720
Don Kirkby <donkirkby@gmail.com>
2023-09-14 02:12:38 +0000

    Add devcontainer to edit markdown and Javascript.

--- docs/Gemfile
+++ docs/Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'jekyll'
 gem 'github-pages', group: :jekyll_plugins
+gem 'webrick'
--- html/serve.sh
+++ html/serve.sh
@@ -1,1 +1,1 @@
-bash -lc "cd ../docs && rvm use 2.7 && bundler exec jekyll serve"
+bash -lc "cd ../docs && rvm use 3.2.2 && bundler exec jekyll serve"

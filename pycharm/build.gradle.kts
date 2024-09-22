d5eb5bcaa7f3083d6006f7ce19b7bd39e04eed4f
Don Kirkby <donkirkby@gmail.com>
2024-09-21 18:27:07 -0700

    Upgrade to Plugin DevKit 2.0, for #627.

--- pycharm/build.gradle.kts
+++ pycharm/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.13.3"
+    id("org.jetbrains.intellij.platform") version "2.0.1"
 }
 
 group = "io.github.donkirkby"
@@ -8,58 +8,20 @@ version = "4.11.4"
 
 repositories {
     mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 // Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("241.14494.240")
-    type.set("IC") // Target IDE Platform: IU for Ultimate, IC for community
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2024.2.2")
 
-    // Pythonid for Ultimate, PythonCore for community
-    plugins.set(listOf("PythonCore:241.14494.240"))
-}
+        plugin("PythonCore:243.15521.24")
 
-sourceSets {
-    main {
-        java {
-            setSrcDirs(listOf("src/main/java"))
-        }
-        resources {
-            setSrcDirs(listOf("src/main/resources", "../plugin/PySrc"))
-        }
-    }
-    test {
-        java {
-            setSrcDirs(listOf("src/test"))
-        }
-    }
-}
-
-tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
-    }
-
-    patchPluginXml {
-        sinceBuild.set("233.11799.241")
-        untilBuild.set("")
-    }
-
-    runPluginVerifier {
-        // Verify against sinceBuild version and current version.
-        ideVersions.set(listOf<String>("233.11799.241", "241.14494.240"))
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
+        pluginVerifier()
     }
 }

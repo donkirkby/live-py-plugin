37087bd9b6f196f9f1cf40c9e7cae6f516547338
Don Kirkby <donkirkby@gmail.com>
2026-01-24 09:38:05 -0800

    Convert to pyproject.toml and update license.
    Closes #751.

--- setup.py
+++ setup.py
@@ -15,30 +15,7 @@ setuptools.setup(
     description=about['__description__'],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=about['__url__'],
     packages=setuptools.find_packages('plugin/PySrc/'),
     package_data={'space_tracer': ['py.typed']},
-    package_dir={'': 'plugin/PySrc/'},
-    entry_points=dict(console_scripts=[
-        'space_tracer = space_tracer:main']),
-    classifiers=[  # from https://pypi.org/classifiers/
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Debuggers",
-        # Synchronize Python versions with py-build.yml workflow and tox.ini.
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console"
-    ],
-    license="MIT",
-    project_urls={
-        'Bug Reports': 'https://github.com/donkirkby/live-py-plugin/issues',
-        'Source': 'https://github.com/donkirkby/live-py-plugin'}
+    package_dir={'': 'plugin/PySrc/'}
 )
